44db173775f50dca76b5102407e6cd71e58b8bd1
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-21 22:48:04 -0300

    feat: questionsRepository

--- src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -23,20 +23,59 @@ export class PrismaQuestionsRepository implements QuestionsRepository {
     return PrismaQuestionMapper.toDomain(question)
   }
 
-    findBySlug(slug: string): Promise < Question | null > {
-      throw new Error("Method not implemented.");
-    }
-    findManyRecent(params: PaginationParams): Promise < Question[] > {
-      throw new Error("Method not implemented.");
-    }
-    create(question: Question): Promise < void> {
-      throw new Error("Method not implemented.");
-    }
-    delete (question: Question): Promise < void> {
-      throw new Error("Method not implemented.");
-    }
-    save(question: Question): Promise < void> {
-      throw new Error("Method not implemented.");
+  async findBySlug(slug: string): Promise<Question | null> {
+    const question = await this.prisma.question.findUnique({
+      where: {
+        slug,
+      },
+    })
+
+    if (!question) {
+      return null
     }
 
+    return PrismaQuestionMapper.toDomain(question)
   }
+
+  async findManyRecent({ page }: PaginationParams): Promise<Question[]> {
+    const questions = await this.prisma.question.findMany({
+      orderBy: {
+        createdAt: 'desc',
+      },
+      take: 20,
+      skip: (page - 1) * 20,
+    })
+
+    return questions.map(PrismaQuestionMapper.toDomain)
+  }
+
+  async create(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.create({
+      data,
+    })
+  }
+
+  async delete(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.delete({
+      where: {
+        id: data.id,
+      },
+    })
+  }
+
+  async save(question: Question): Promise<void> {
+    const data = PrismaQuestionMapper.toPrisma(question)
+
+    await this.prisma.question.update({
+      where: {
+        id: question.id.toString(),
+      },
+      data,
+    })
+  }
+
+}
