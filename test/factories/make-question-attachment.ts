3e4c312cb383ae5b98539e4a68ad1e582fbf0ee1
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-05-29 21:40:44 -0300

    feat: editando perguntas com anexos

--- test/factories/make-question-attachment.ts
+++ test/factories/make-question-attachment.ts
@@ -1,5 +1,7 @@
+import { Injectable } from "@nestjs/common";
 import { UniqueEntityID } from "src/core/entities/unique-entity-id";
 import { QuestionAttachment, QuestionAttachmentProps } from 'src/domain/forum/enterprise/entities/question-attachment';
+import { PrismaService } from "src/infra/database/prisma/prisma.service";
 
 export function makeQuestionAttachment(
   override?: Partial<QuestionAttachmentProps>,
@@ -14,4 +16,26 @@ export function makeQuestionAttachment(
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {},
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: {
+        id: questionAttachment.attachmentId.toString(),
+      },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
